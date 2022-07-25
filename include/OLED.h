8a06e2694de5156c7a63511ae098176552f00684
Jason <jasonabeckmann@gmail.com>
2022-07-25 03:14:16 -0400

    UI drawing

--- include/OLED.h
+++ include/OLED.h
@@ -8,8 +8,34 @@ class OLED {
     public:
         OLED();
 
+        /**
+         * @brief Draws and displays the splash screen
+         * 
+         */
         void drawSplashScreen();
 
+        /**
+         * @brief Draws and displays the UI with all the important info
+         * 
+         */
+        void drawUI();
+        
+        void setBoardBattery(float level);
+        void setRemoteBattery(float level);
+        void setThrottle(float level);
+
+        /**
+         * @brief Set the Speed object
+         * 
+         * @param speed 
+         */
+        void setSpeed(float speed);
+
     private:
-        Adafruit_SSD1306 display;
+        float _boardBattery = 0.0;
+        float _remoteBattery = 0.0;
+        float _throttle = 0.0;
+        float _speed = 0.0;
+
+        Adafruit_SSD1306 _display;
 };
